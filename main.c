dbdfec0328b3b971d02ec1cb31a27b662a830b58
nevesm <44535389+nevesm@users.noreply.github.com>
2021-06-01 19:22:41 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,19 @@
+int main()
+{
+    printf("FACULDADE METROPOLITANAS UNIDAS");
+    printf(" \n");
+    printf(" \n");
+    printf("APS - LOGICA DE PROGRAMACAO - 1º SEMESTRE");
+    printf(" \n");
+    printf(" \n");
+    printf("Desenvolvedor: Marcus Neves\n");
+    printf("RA: 6932681\n");
+    printf("Contato: marcus-neves@outlook.com\n");
+    printf(" \n");
+    printf("*****************\n");
+    printf("*fmu-soma-e-raiz*\n");
+    printf("*****************\n");
+    printf(" \n");
+    printf("Menu de opções:\n");
+    return 0;
+}
