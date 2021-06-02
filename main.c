de07bdaf45126c4a59cdd755c16af8ce41649b57
nevesm <44535389+nevesm@users.noreply.github.com>
2021-06-01 22:36:55 -0300

    first commit

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
 int main()
 {
+    double opcao;
+    double n1;
+    double n2;
+    double numeroRaiz;
+    double resultadoRaiz;
+
     printf("FACULDADE METROPOLITANAS UNIDAS");
     printf(" \n");
     printf(" \n");
@@ -14,6 +23,31 @@ int main()
     printf("*fmu-soma-e-raiz*\n");
     printf("*****************\n");
     printf(" \n");
-    printf("Menu de opções:\n");
-    return 0;
+    printf("MENU PRINCIPAL\n");
+    printf(" \n");
+    printf("1 - Somar dois números\n");
+    printf("2 - Saber a raiz quadrada de um número\n");
+    printf(" \n");
+    printf(" \n");
+    printf("Digite sua opção:");
+    scanf("%lf",&opcao);
+
+    if(opcao == 2) {
+          printf("\nCALCULANDO RAÍZ QUADRADA\n");
+          printf("Digite um número: ");
+          scanf("%lf",&numeroRaiz);
+          printf("RESULTADO: %f",resultadoRaiz = sqrt(numeroRaiz));
+      }
+      if(opcao == 1) {
+          printf("\nSOMANDO DOIS NÚMEROS");
+          printf("\nDigite o primeiro número: ");
+          scanf("%lf",&n1);
+          printf(" \n");
+          printf("\nDigite o segundo número: ");
+          scanf("%lf",&n2);
+          printf("-----------------------------\n");
+          printf("RESULTADO: %f\n", n1 + n2);
+          printf("-----------------------------\n");
+      } 
+      
 }
